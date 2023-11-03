ada9e3be6df452340ac278d30284f4801f54a91c
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-03 16:10:11 -0700

    Add displayPizzaDetails UI.

--- js/scripts.js
+++ js/scripts.js
@@ -48,6 +48,7 @@ window.addEventListener("load", () => {
 
 function handleSubmit(e) {
     e.preventDefault();
+    document.querySelector("div#orderDetails").classList.add("hidden");
     const userToppingChoices = document.querySelectorAll('input[type="checkbox"]');
     const userSizeChoice = document.querySelector('input[type="radio"]:checked');
     const displayResult = document.querySelector("h2#displayPrice");
@@ -70,16 +71,27 @@ function handleSubmit(e) {
 function listOrderSummary(cart) {
     let orderSummary = document.querySelector("div#orderSummary");
     orderSummary.innerText = null;
+    // const h3
     const ul = document.createElement("ul");
     Object.values(cart.pizzas).forEach((pizza) => {
             const li = document.createElement("li");
             li.append(pizza.size);
             li.setAttribute("id", pizza.id);
             ul.append(li);
-
+            // Event listener for clicks on list items
             li.addEventListener("click", () => {
                 displayPizzaDetails(pizza.id);
-        });
+            });
     });
     orderSummary.append(ul);
+}
+
+function displayPizzaDetails(pizzaId) {
+    document.querySelector("h2#displayPrice").innerText = "Current Pizza's:";
+    const pizza = cart.getPizza(pizzaId);
+    document.querySelector("span#size").innerText = pizza.size;
+    document.querySelector("span#toppings").innerText = pizza.toppings.join(", ");
+    document.querySelector("span#price").innerText = pizza.price();
+    document.querySelector("span#pizzaNumber").innerText = pizzaId + 1;
+    document.querySelector("div#orderDetails").removeAttribute("class");
 }
